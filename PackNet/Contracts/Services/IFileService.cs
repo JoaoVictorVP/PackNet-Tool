e83bd2872fa39b89d13ccd516ed79e70dcb5eb96
J. V - N <joaovitorsl2012@hotmail.com>
2022-07-01 11:58:18 -0300

    adding file service

---
+++ PackNet/Contracts/Services/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackNet.Contracts.Services;
+
+public interface IFileService
+{
+    string Search(string pattern, string inDir = "./", bool deep = false);
+    IEnumerable<string> SearchMany(string pattern, string inDir = "./", bool deep = false);
+}
---
+++ PackNet/Core/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using PackNet.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackNet.Core.Services;
+
+public class FileService : IFileService
+{
+    public string Search(string pattern, string inDir = "./", bool deep = false)
+    {
+        return DoSearch(pattern, inDir, deep).FirstOrDefault() ?? "";
+    }
+
+    public IEnumerable<string> SearchMany(string pattern, string inDir = "./", bool deep = false)
+    {
+        return DoSearch(pattern, inDir, deep);
+    }
+
+    static IEnumerable<string> DoSearch(string pattern, string inDir, bool deep)
+    {
+        var files = Directory.EnumerateFiles(inDir, pattern, deep ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+        return files;
+    }
+}
